24421747c6ac854e7d422038ade84737a373c0a9
Elayne Argollo <48841005+elayneargollo@users.noreply.github.com>
2020-04-06 13:48:40 -0300

    comentario e conceito basico

---
+++ exercicios-js/Fundamentos/organizacao.js
@@ -0,0 +1,12 @@
+/* Primeira aulda do curso web moderno */
+
+console.log('Sentenca de codigo que pode ou nao terminar com ponto e virgula e bloco de codigo.')
+
+    {
+        /* bloco de codigo (agrupa sentenca de codigo) */
+        /* estrutura de controle */
+
+        console.log(
+            'Bloco', 
+            'de codigo.')
+    }
