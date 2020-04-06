74fec1ac5324d015ddd8805ee6c005328dc37dd5
Elayne Argollo <48841005+elayneargollo@users.noreply.github.com>
2020-04-06 14:01:41 -0300

    variaveis e constantes

---
+++ exercicios-js/Fundamentos/variaveis&constantes.js
@@ -0,0 +1,15 @@
+/* basico de Var, Let e Const */
+
+/* variaveis */
+var linguagem = 'JS'  /* pode ser redeclarar */
+let curso = 'web' /* mais moderno e so pode declarar uma unica vez, logo, mais seguro */
+
+var liguangem = 'JavaScript'
+curso = 'web-moderno'
+
+console.log(linguagem, curso)
+
+/* constante */
+const ano = 2020  /* nao pode ser alterada */
+console.log(ano)
+
