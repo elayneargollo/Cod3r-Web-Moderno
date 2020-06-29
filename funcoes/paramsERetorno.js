fe0ed31f4b768d4d9860ace2560d56f6b6e7372f
elayneargollo <elayne.natalia@outlook.com>
2020-06-28 21:56:55 -0300

    funçoes com ou sem retorno

---
+++ funcoes/paramsERetorno.js
@@ -0,0 +1,9 @@
+function area(largura, altura){
+    const area = largura * altura
+    
+    if(area > 20 ){
+        console.log('Valor acima do permitido')
+    } else {
+        return area
+    }
+}
