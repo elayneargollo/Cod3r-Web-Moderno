825ffe651a86f7501b21da52c845732f2dec6977
elayneargollo <elayne.natalia@outlook.com>
2020-06-30 17:57:53 -0300

    Arrow function

---
+++ funcoes/arrowFunction.js
@@ -0,0 +1,21 @@
+let dobro = function (a){
+    return 2*a;
+}
+
+// arrow function
+dobro = (a) => {
+    return 2*a
+}
+
+// arrow function reduzida
+dobro = a => 2 * a;
+
+let ola = function () {
+    return 'Olá'
+}
+
+// forma padrão
+ola = () => 'Ola'
+
+// existe um parametro mas posso ignora-lo se quiser
+ola = _ => 'Olá'
