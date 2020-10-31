46a831beef81652a677c15d48b3443189790f26a
elayneargollo <elayne.natalia@outlook.com>
2020-10-31 16:56:58 -0300

    remove trash

--- angular-exercicio/card/src/app/app.module.ts
+++ angular-exercicio/card/src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoMaterialModule } from './material.component';
-import { TextoComponent } from './component/texto/texto.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TextoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
